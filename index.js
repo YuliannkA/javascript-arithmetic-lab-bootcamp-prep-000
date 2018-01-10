69b09b7f7c5b259564f28c7563ac1bcfba9c5132
Yulia Drozdova <yuliannka@gmail.com>
2018-01-10 22:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function dec(n) {
 }
 
 function makeInt(n) {
-var n = parseInt(n)
-  if ( n = 0 ) {
-    return 0
-    } else {
-      return n
-      }
+  var m = parseInt(n);
+    if ( m === 0 ) {
+      return 0
+      } else {
+        return parseInt(n)
+        }
 }
